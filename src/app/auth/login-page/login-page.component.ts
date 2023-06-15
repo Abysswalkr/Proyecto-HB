a439c17e0adce8d771a434b320ab45891aa1e178
Angel <angelherrarte3@gmail.com>
2023-06-15 15:10:54 -0600

    Agregar login

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
+  imports: [RouterModule],
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
