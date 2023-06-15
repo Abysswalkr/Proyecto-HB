13b47892cdf167621934eae36aa77e3df404030f
Angel <angelherrarte3@gmail.com>
2023-06-15 15:11:03 -0600

    Agregar registro

--- src/app/auth/register-page/register-page.component.ts
+++ src/app/auth/register-page/register-page.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
+  imports: [RouterModule],
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
