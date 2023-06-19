08557538c0bdf725037acbaba0784411e7e4af40
Angel <angelherrarte3@gmail.com>
2023-06-18 22:50:25 -0600

    Actualizar servicio de autenticacion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,26 +1,73 @@
 import { Injectable } from '@angular/core';
-import { AuthChangeEvent, AuthSession, Session, SupabaseClient } from '@supabase/supabase-js';
+import { AuthApiError, SignUpWithPasswordCredentials, SupabaseClient, User } from '@supabase/supabase-js';
+import { BehaviorSubject } from 'rxjs';
 import { Supabase } from 'src/models/supabase.client';
+
+export interface SignUpParams {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private client: SupabaseClient = Supabase.getInstance();
-  _session: AuthSession | null = null;
+  private userSubject = new BehaviorSubject<User | null>(null);
 
   constructor() { }
+  
+  private setSession() {
+    const session = localStorage.getItem('session') as User | null;
+    if (session) this.userSubject.next(session as User);
+  }
+  
+  get user() {
+    return this.userSubject.asObservable();
+  }
+  
+  async login(email: string, passowrd: string) : Promise<User | AuthApiError> {
+    try {
+      const { data, error } = await this.client.auth.signInWithPassword({
+        email: email, password: passowrd
+      });
+      if (error) throw error;
+      
+      this.setSession();
+      return data.user
 
-  authChanges(callback: (event: AuthChangeEvent, session: Session | null) => void) {
-    return this.client.auth.onAuthStateChange(callback)
+    } catch (error) {
+      return error as AuthApiError;
+    }
   }
 
-  signIn(email: string, passowrd: string) {
-    return this.client.auth.signInWithPassword({
-      email: email, password: passowrd
-    });
-  }
+  async register({ email, password, firstName, lastName }: SignUpParams) {
+    const params: SignUpWithPasswordCredentials = {
+      email: email,
+      password: password,
+      options: {
+        data: {
+          first_name: firstName,
+          last_name: lastName
+        }
+      }
+    }
 
+    try {
+      const { data, error } = await this.client.auth.signUp(params);
+      if (error) throw error;
+      
+      this.setSession();
+      return data.user
+
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  }
+  
   signOut() {
     return this.client.auth.signOut()
   }
